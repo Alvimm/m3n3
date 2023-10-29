81ae4da0621eabcc3bcf794958d6a357cc9ebf47
Filipe Alvim <filipe2012alvim@gmail.com>
2023-10-29 13:54:09 -0300

    implement pessoas class

--- 1º procedimento/CadastroBD/src/cadastrobd/model/Pessoas.java
+++ 1º procedimento/CadastroBD/src/cadastrobd/model/Pessoas.java
@@ -9,5 +9,34 @@ package cadastrobd.model;
  * @author Filipe
  */
 public class Pessoas {
+    private int id;
+    private String nome;
+    private String logradouro;
+    private String cidade;
+    private String estado;
+    private String telefone;
+    private String email;
+    
+    public Pessoas(){}
+
+    public Pessoas(int id, String nome, String logradouro, String cidade, String estado, String telefone, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.logradouro = logradouro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.telefone = telefone;
+        this.email = email;
+    }
+    
+    public void exibir(){
+        System.out.println("Id: " + id);
+        System.out.println("Nome: " + nome);
+        System.out.println("Logradouro: " + logradouro);
+        System.out.println("Cidade: " + cidade);
+        System.out.println("Estado: " + estado);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("E-mail: " + email);
+    }
     
 }
