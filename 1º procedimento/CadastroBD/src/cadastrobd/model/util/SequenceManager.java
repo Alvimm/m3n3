a54e3d27a8e45874da23194c00e3035ec959c3f8
Filipe Alvim <filipe2012alvim@gmail.com>
2023-10-29 16:37:22 -0300

    implement SequenceManager

--- 1º procedimento/CadastroBD/src/cadastrobd/model/util/SequenceManager.java
+++ 1º procedimento/CadastroBD/src/cadastrobd/model/util/SequenceManager.java
@@ -3,11 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package cadastrobd.model.util;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  *
  * @author Filipe
  */
 public class SequenceManager {
+    private ConectorBD conn;
     
+    public SequenceManager(ConectorBD conn){
+        this.conn = conn;
+    }
+    
+    public int getValue(String sequenceName) throws SQLException{
+        int value = 0;
+        String sql = "SELECT nextval('" + sequenceName + "')";
+        
+        try{
+            ResultSet resultSet = conn.getSelect(sql);
+            if(resultSet.next()){
+                value = resultSet.getInt(1);
+            }
+            conn.close(resultSet);
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return value;
+    }
 }
