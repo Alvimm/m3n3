846a1bab4b4e8b732dbc6deaee04e2911a0fc941
Filipe Alvim <filipe2012alvim@gmail.com>
2023-10-29 14:08:14 -0300

    implement pessoaFisica class

--- 1º procedimento/CadastroBD/src/cadastrobd/model/PessoaFisica.java
+++ 1º procedimento/CadastroBD/src/cadastrobd/model/PessoaFisica.java
@@ -8,6 +8,18 @@ package cadastrobd.model;
  *
  * @author Filipe
  */
-public class PessoaFisica {
+public class PessoaFisica extends Pessoas{
+    private String cpf;
+    public PessoaFisica(){}
+    public PessoaFisica(String cpf, int id, String nome, String logradouro, String cidade, String estado, String telefone, String email) {
+        super(id, nome, logradouro, cidade, estado, telefone, email);
+        this.cpf = cpf;
+    }
+    
+    @Override
+    public void exibir(){
+        super.exibir();
+        System.out.println("CPF: " + cpf);
+    }
     
 }
